b0360a711af2df5fa4f9c53b590f2b7d0bd74199
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2024-05-19 11:10:33 +0600

    service section style modified

--- src/Components/Services/DemoService.jsx
+++ src/Components/Services/DemoService.jsx
@@ -14,19 +14,19 @@ const DemoService = () => {
             <h1 className='text-5xl font-semibold text-center my-10'>Services</h1>
 
             <Flicking circular={true} plugins={plugins}>
-                <div className="card-panel w-96">
-                    <img src={fullStackIcon} className='w-full h-64' alt="" />
-                    <h1 className='text-2xl'>Full Stack Web Development</h1>
+                <div className="card-panel w-96 shadow-xl p-3 text-center mb-10 bg-white rounded-xl">
+                    <img src={fullStackIcon} className='w-full h-64 rounded-xl' alt="" />
+                    <h1 className='text-2xl font-semibold'>Full Stack Web Development</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolor maxime quisquam nulla non laboriosam, quas quis at reiciendis molestiae tempore quidem a obcaecati fuga omnis corporis natus, placeat cupiditate.</p>
                 </div>
-                <div className="card-panel w-96">
-                    <img src={UiUxIcon} className='w-full h-64' alt="" />
-                    <h1 className='text-2xl'>UI/UX Design</h1>
+                <div className="card-panel w-96 shadow-xl p-3 text-center mb-10 bg-white rounded-xl">
+                    <img src={UiUxIcon} className='w-full h-64 rounded-xl' alt="" />
+                    <h1 className='text-2xl font-semibold'>UI/UX Design</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolor maxime quisquam nulla non laboriosam, quas quis at reiciendis molestiae tempore quidem a obcaecati fuga omnis corporis natus, placeat cupiditate.</p>
                 </div>
-                <div className="card-panel w-96">
-                    <img src={mobileAppIcon} className='w-full h-64' alt="" />
-                    <h1 className='text-2xl'>Mobile App Development</h1>
+                <div className="card-panel w-96 shadow-xl p-3 text-center mb-10 bg-white rounded-xl">
+                    <img src={mobileAppIcon} className='w-full h-64 rounded-xl' alt="" />
+                    <h1 className='text-2xl font-semibold'>Mobile App Development</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolor maxime quisquam nulla non laboriosam, quas quis at reiciendis molestiae tempore quidem a obcaecati fuga omnis corporis natus, placeat cupiditate.</p>
                 </div>
             </Flicking>
