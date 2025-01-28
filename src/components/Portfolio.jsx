83a1a2117311e0c7fbfc66b5da160aa782185164
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2025-01-29 01:18:01 +0600

    updated portfolio page

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,11 +3,6 @@ import { projects } from '../../utils/projects';
 import TitleContainer from './TitleContainer';
 
 const Portfolio = () => {
-
-    const onclickHandler = (path) => {
-        window.location.href = path;
-    };
-
     return (
         <div className='md:w-9/12 mx-auto space-y-10'>
 
@@ -16,19 +11,61 @@ const Portfolio = () => {
                 subTitle='Showcasing some of my best work'
             />
 
-            <div className='grid grid-cols-1 md:grid-cols-3 gap-8'>
-                {projects.map(project =>
-                    <div key={project?.id} className="image-card" >
-                        <img src={project.image} alt='' />
-                        <div className="overlay  " >
-                            <div>
-                                <h1 className='text-2xl'>{project.title}</h1>
-                                <p className='cursor-pointer' onClick={() => onclickHandler(project.path)}>Visit Here</p>
+            <div className='flex flex-col gap-20 font-montserrat'>
+                {projects.map((project, index) =>
+                    <div
+                        key={project?.id}
+                        className={`flex ${index % 2 === 0 ? 'flex-row' : 'flex-row-reverse'} items-center gap-5`}
+                    >
+                        <img
+                            src={project.image}
+                            alt='project image'
+                            className='h-96 w-1/2'
+                        />
+
+                        <div className='space-y-2 w-1/2'>
+                            <p className='text-xl font-thin'>{project?.category}</p>
+                            <h1 className='text-lg font-semibold sm:text-3xl'>{project.title}</h1>
+
+                            <div className='border border-[#5a5555] rounded bg-black p-5'>
+                                <p>{project?.description}</p>
                             </div>
+
+                            <ul className='flex flex-wrap gap-5'>
+                                {project?.techStacks.map((stack, index) =>
+                                    <li key={index}>{stack}</li>
+                                )}
+                            </ul>
+
+                            <div className='flex gap-10'>
+                                <a
+                                    className='bg-white text-black hover:bg-black hover:text-white border px-5 py-2'
+                                    href={project?.liveLink}
+                                    target='_blank'
+                                >Live Website</a>
+
+                                {project?.githubClient && <a
+                                    className='bg-white text-black hover:bg-black hover:text-white border px-5 py-2'
+                                    href={project?.githubClient}
+                                    target='_blank'
+                                >Frontend Code</a>}
+
+                                {project?.githubServer && <a
+                                    className='bg-white text-black hover:bg-black hover:text-white border px-5 py-2'
+                                    href={project?.githubServer}
+                                    target='_blank'
+                                >Backend Code</a>}
+                            </div>
+
                         </div>
                     </div>
                 )}
             </div>
+
+            <div>
+
+            </div>
+
         </div>
     );
 };
--- utils/projects.js
+++ utils/projects.js
@@ -1,38 +1,38 @@
 import project1 from '../src/assets/projects/1.png';
 import project2 from '../src/assets/projects/2.png';
 import project3 from '../src/assets/projects/3.png';
-import project4 from '../src/assets/projects/4.png';
-import project5 from '../src/assets/projects/5.png';
 
 export const projects = [
     {
         id: 1,
-        title: 'Bongo Explorer',
+        title: 'Bongo Explorer | Tour & Travel',
         image: project1,
-        path: 'https://bongo-explorer.web.app/'
+        description: 'A complete travel guide online application based on React that connects visitors, tour guides, and administrators. It has a multi-role system with separate dashboards for admins, guides, and travellers. Administrators have complete control over tour packages, guide requests, and other details. Guides may view and accept tour assignments, while tourists can browse various packages, choose their preferred excursions, and select guides. The platform also includes a secure payment mechanism for seamless transactions, as well as a strong authentication system to assure secure access for each role.',
+        category: 'FULL-STACK',
+        techStacks: ['React', 'Tailwing CSS', 'Ant Design', 'Firebase', 'Express JS', 'MongoDB'],
+        liveLink: 'https://bongo-explorer.web.app',
+        githubClient: 'https://github.com/Sadi73/bongo-explorer-client-side',
+        githubServer: 'https://github.com/Sadi73/bongo-explorer-server-side',
     },
     {
         id: 2,
-        title: 'Banu & Haque Homes',
+        title: 'Cosystay | Hotel Management',
         image: project2,
-        path: 'https://banu-and-haque-homes.web.app/'
+        description: 'A web application for hotel employees and customers that manages hotel rooms and bookings. The app features a simple dashboard where hotel staff can add new rooms, handle reservations, and track guest information. The platform also allows guests to browse available rooms and make reservations. It makes it simple and effective for hotel personnel and guests to manage hotel operations and reservations.',
+        category: 'FULL-STACK',
+        techStacks: ['Next JS', 'Tailwing CSS', 'Material UI', 'Express JS', 'MongoDB', 'Node Mailer', 'Socket.io'],
+        liveLink: 'https://hotel-booking-system-gilt.vercel.app',
+        githubClient: 'https://github.com/Sadi73/hotel-booking-system',
+        githubServer: 'https://github.com/Sadi73/hotel-booking-system-server',
     },
     {
         id: 3,
-        title: "Esha's Craft",
+        title: 'Banu & Haque Homes',
         image: project3,
-        path: 'https://esha-craft.web.app/'
-    },
-    {
-        id: 4,
-        title: 'EService BD',
-        image: project4,
-        path: 'https://eservicebd-3433d.web.app/'
-    },
-    {
-        id: 5,
-        title: 'Smart Ticket',
-        image: project5,
-        path: 'https://resilient-unicorn-a9662b.netlify.app/'
+        description: 'A simple real estate web application where users can browse a list of properties available for sale or rent. By clicking on a property, users can view detailed information about it. The platform also includes an authentication system to ensure secure access for users.',
+        category: 'FRONT-END',
+        techStacks: ['React', 'Tailwing CSS', 'Firebase'],
+        liveLink: 'https://banu-and-haque-homes.web.app',
+        githubClient: 'https://github.com/Sadi73/banu-and-haque-homes',
     },
 ];
